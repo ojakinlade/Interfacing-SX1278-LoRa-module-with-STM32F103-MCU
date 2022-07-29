35a516c07d1de7aa0e484e67e00c740ca9123509
Joseph Akinlade <42944862+JosephAkinlade@users.noreply.github.com>
2022-07-29 15:15:24 -0700

    Delete SX1278_LoRa_Code directory

--- SX1278_LoRa_Code/Objects/main.d
+++
@@ -1,7 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: sx1278.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\main.o: systick.h
--- SX1278_LoRa_Code/spi.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_SPI_H
-#define INC_SPI_H
-
-extern void SPI1_Init(void);
-extern void SPI1_TransmitByte(uint8_t byte);
-extern void SPI1_TransmitBytes(uint8_t* data, uint8_t len);
-extern void SPI1_ReceiveBytes(uint8_t* data, uint32_t len);
-
-#endif /* INC_SPI_H */
